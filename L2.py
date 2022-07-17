797b2bee7ece2a4663c8d8fbe7a11ad75e3dec68
debachaks <88777152+debachaks@users.noreply.github.com>
2022-07-17 15:13:43 +0530

    Create L2.py

---
+++ L2.py
@@ -0,0 +1,42 @@
+#Print names n times , TC:O(N),SC:O(N)
+def f(i,n):
+    #base case
+    if i > n:
+        return
+    print("Debaleena")
+    f(i+1,n)
+
+n = int(input("Enter a number "))   
+f(1,n)
+
+
+#Print 1 to n
+def f2(i,n):
+    #base case
+    if i > n:
+        return
+    print(i)
+    f2(i+1,n)
+
+n = int(input("Enter a number: "))
+f2(1,n)
+
+#print from 1 to n using backtracking
+def f2_back(i,n):
+    if i<1:
+        return
+    f2_back(i-1,n)
+    print(i)
+    
+n = int(input("Enter a number: "))
+f2_back(n,n)
+
+#print from n to 1
+def f3(n):
+    if n<1:
+        return
+    print(n)
+    f3(n-1)
+
+n = int(input("Enter a number: "))
+f3(n)    
